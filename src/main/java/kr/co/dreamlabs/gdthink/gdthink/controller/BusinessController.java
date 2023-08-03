aae768d5ae2c7c88ced5ae1ecd0bd4fa4bc2cd9d
wogus <wogus@DESKTOP-N5ML8CM>
2023-08-03 16:57:02 +0900

    테스트 소스 삭제

--- src/main/java/kr/co/dreamlabs/gdthink/gdthink/controller/BusinessController.java
+++ src/main/java/kr/co/dreamlabs/gdthink/gdthink/controller/BusinessController.java
@@ -6,10 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BusinessController {
 
-	@GetMapping("/test2")
-	public String test2() {
-		return "Business/test2.html";
-	}
 	@GetMapping("/history2")
 	public String history() {
 		return "Business/history2.html";
