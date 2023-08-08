11ea6254c21b798f0d6205d61770ed8c9be0d28d
wogus <wogus@DESKTOP-N5ML8CM>
2023-08-08 15:45:50 +0900

    c

--- src/main/java/kr/co/dreamlabs/gdthink/gdthink/service/MenuService.java
+++ src/main/java/kr/co/dreamlabs/gdthink/gdthink/service/MenuService.java
@@ -1,5 +1,21 @@
 package kr.co.dreamlabs.gdthink.gdthink.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.dreamlabs.gdthink.gdthink.dao.TbMenuDAO;
+import kr.co.dreamlabs.gdthink.gdthink.vo.TbMenuVo;
+
+@Service
 public class MenuService {
 
+	@Autowired
+	TbMenuDAO tbMenuDao;
+	
+	public List<TbMenuVo> getAllMenu(){
+		return tbMenuDao.getAllMenu();
+	}
+	
 }
