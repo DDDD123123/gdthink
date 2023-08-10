efcdfe5330078ea227e2b55fc497528eb690ef39
wogus <wogus@DESKTOP-N5ML8CM>
2023-08-10 17:32:39 +0900

    merge

--- src/main/java/kr/co/dreamlabs/gdthink/gdthink/GdthinkApplication.java
+++ src/main/java/kr/co/dreamlabs/gdthink/gdthink/GdthinkApplication.java
@@ -2,11 +2,18 @@ package kr.co.dreamlabs.gdthink.gdthink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @EnableCaching
 @SpringBootApplication
-public class GdthinkApplication {
+public class GdthinkApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GdthinkApplication.class);
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GdthinkApplication.class, args);
